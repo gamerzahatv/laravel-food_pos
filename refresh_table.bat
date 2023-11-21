be722ab7c7643738b582c84f43486fd184f6d7c1
MEOWNANI <gamerzahatv@gmail.com>
2023-11-22 02:50:17 +0700

    delete script

--- refresh_table.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-php artisan migrate:fresh --seed
-php artisan permission:create-role admin
-php artisan permission:create-role User
-pause
--- start_dev.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-php artisan serve --port=12123
-pause
